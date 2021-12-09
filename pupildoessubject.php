5e86a01d49dff3f570eec3a2e9b026b0ef2f8138
Noovsky <stephan1katank@gmail.com>
2021-12-09 10:49:05 +0000

    h

---
+++ pupildoessubject.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    
+    
+    <title>Page title</title>
+    
+</head>
+<body>
+<form action="addtoclass.php" method = "post">
+
+<select name = "student">
+<?php
+include_once('connection.php');
+$stmt = $conn->prepare("SELECT * FROM TblUsers WHERE Role = 0 ORDER BY Surname ASC");
+$stmt->execute();
+
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+	echo('<option value='.$row["UserID"].'>'.$row["Surname"].', '.$row["Forename"].'</option>');
+
+}
+?>
+</select>
+<select name = "subject">
+<?php
+include_once('connection.php');
+$stmt = $conn->prepare("SELECT * FROM TblUsers WHERE Role = 0 ORDER BY Surname ASC");
+$stmt->execute();
+
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+	echo('<option value='.$row["UserID"].'>'.$row["Surname"].', '.$row["Forename"].'</option>');
+
+}
+?>
+</select>
+
+<input type="submit" value="Add User">
+</form>
+     
+</body>
+</html>
+
+
+
+
+
